4bde2e0b7b3e5f33fb5994ad3b7368836fdc8061
Trippie-Redd <anma07002@utb.vaxjo.se>
2025-03-21 13:58:23 +0100

    Sista

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -3,11 +3,9 @@ let darkModeOn = false;
 function switchTheme(element) {
     if (!darkModeOn) {
         document.body.classList.add('dark-mode');
-        localStorage.setItem('theme', 'dark-mode');
         darkModeOn = true;
     } else {
         document.body.classList.remove('dark-mode');
-        localStorage.setItem('theme', '');
         darkModeOn = false;
     }   
 }
